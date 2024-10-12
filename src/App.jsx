24e21b8b34375589f5ad8f1c769fc7999492a332
Luciano.Reis <lrr.arms233@gmail.com>
2024-10-12 20:00:18 -0300

    Update NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logoImg from './assets/logo.jpeg'
+import Navbar from './components/Navbar/Navbar'
 
 import './styles/App.css'
 
@@ -10,30 +10,7 @@ class App extends React.Component {
   // método responsável por renderizar o conteúdo HTML do nosso componente
   render() {
     return (
-      <header>
-        <nav id="navbar">
-          <div className="nav-brand">
-            <img id="brand" src={logoImg} alt="" />
-            <h1>Space Flight News</h1>
-          </div>
-
-          <ul className="nav-list">
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/">Trending</a>
-            </li>
-            <li>
-              <a href="/">Categories</a>
-            </li>
-            <li>
-              <a href="/">About us</a>
-            </li>
-          </ul>
-
-        </nav>
-      </header>
+     <Navbar />
     );
   }
 }
