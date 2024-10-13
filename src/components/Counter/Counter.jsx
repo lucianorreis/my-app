38864ad088d6d05dee3dd2f6e9531f7b6afda24e
Luciano.Reis <lrr.arms233@gmail.com>
2024-10-13 12:59:56 -0300

    Update app.jsx

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,16 +1,40 @@
 import React from "react";
 
 export class Counter extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>0</h1>
+  constructor() {
+    super();
+    // this.contador = 0;
+    this.state = { contador: 0 };
+  }
 
-            <div>
-                <button>Diminuir</button>
-                <button>Aumentar</button>
-            </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div
+        style={{
+          marginTop: "20px",
+          marginLeft: "20px",
+        }}
+      >
+        <h1>{this.state.contador}</h1>
+
+        <div>
+            <button
+                onClick={() => {
+                    //! this.state.contador -= 1
+                    this.setState({ contador: this.state.contador - 1 });
+            }}
+            >
+                Diminuir
+            </button>
+            <button
+                onClick={() => {
+                    this.setState({ contador: this.state.contador + 1 });
+                }}
+            >
+                Aumentar
+            </button>
+        </div>
+      </div>
+    );
+  }
 }
